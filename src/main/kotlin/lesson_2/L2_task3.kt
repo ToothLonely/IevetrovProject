4dfd71b3331cf80ca85320b9e1d2d781d80a56e3
ToothLonely <arsen.shirinov2004@gmail.com>
2024-11-29 19:02:04 +0300

    OnB-2-1 calculated train arrive time

---
+++ src/main/kotlin/lesson_2/L2_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_2
+
+const val MINUTES_IN_HOUR = 60
+
+fun main() {
+    // Инициализация переменных с начальными данными
+    var hours: Byte = 9
+    var minutes: Byte = 39
+    val travelTime: Short = 457
+
+    // Перевод в минуты времени отправки и времени прибытия
+    val departTimeInMinutes: Int = hours * MINUTES_IN_HOUR + minutes
+    val arriveTimeInMinutes: Int = departTimeInMinutes + travelTime
+
+    // Перевод в часы и минуты для корректного отображения в формате времени
+    minutes = (arriveTimeInMinutes / MINUTES_IN_HOUR).toByte()
+    hours = (arriveTimeInMinutes % MINUTES_IN_HOUR).toByte()
+
+    println(String.format("%02d:%02d", hours, minutes))
+}
