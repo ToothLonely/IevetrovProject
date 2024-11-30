483e11689c1adde35aec66689fd3c69aaa967c5e
ToothLonely <arsen.shirinov2004@gmail.com>
2024-11-30 11:22:07 +0300

    OnB-2-2 delete unnecessary comments

--- src/main/kotlin/lesson_2/L2_task2.kt
+++ src/main/kotlin/lesson_2/L2_task2.kt
@@ -1,13 +1,12 @@
 package org.example.lesson_2
 
 fun main() {
-    //Объявление и инициализация переменных
     val numberOfEmployees: Byte = 50
     val numberOfInterns: Byte = 30
     val internSalary: Short = 20_000
     val employeeSalary: Short = 30_000
 
-    //Вычисление значений
+    //Вычисление кол-ва рабочих и расходов на них
     val expensesOnEmployees: Int = employeeSalary * numberOfEmployees
     val expensesOnInterns: Int = internSalary * numberOfInterns
     val allExpenses: Int = expensesOnInterns + expensesOnEmployees
