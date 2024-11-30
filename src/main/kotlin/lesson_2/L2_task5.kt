5e398392dfbc2a4c423371aeefbc42309265576c
ToothLonely <arsen.shirinov2004@gmail.com>
2024-12-01 01:17:47 +0300

    OnB-2-5 delete comment and fixed percent

--- src/main/kotlin/lesson_2/L2_task5.kt
+++ src/main/kotlin/lesson_2/L2_task5.kt
@@ -3,14 +3,13 @@ package org.example.lesson_2
 import kotlin.math.pow
 
 fun main() {
-    // Sn = S0 * (1 + p/100)**n
     val startSum: Int = 70_000
-    val percent: Double = 0.167
+    val percent: Double = 16.7
     val numberOfYears: Byte = 20
     val targetSum: Double
 
     //Вычисление финальной суммы
-    targetSum = startSum * (1 + percent).pow(numberOfYears.toInt())
+    targetSum = startSum * (1 + percent / 100).pow(numberOfYears.toInt())
 
     //Стоит ли записывать форматированную строку в отдельную переменную, как я делал до этого?
     println(String.format("%.3f", targetSum))
