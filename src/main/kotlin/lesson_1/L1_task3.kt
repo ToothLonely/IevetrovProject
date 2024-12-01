b8011ad176106b5f6c01d33bce47705ca67f5a05
ToothLonely <arsen.shirinov2004@gmail.com>
2024-12-01 10:05:52 +0300

    OnB-1-3 fixed type for date and time

--- src/main/kotlin/lesson_1/L1_task3.kt
+++ src/main/kotlin/lesson_1/L1_task3.kt
@@ -2,17 +2,16 @@ package lesson_1
 
 fun main() {
 
-    val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(year)
-    println(String.format("%02d", hour))
-    println("0$minute") // не знал какой вариант надо тут использовать,
-    // поэтому просто использовал оба варианта
+    println(hour)
+    println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("$hour:$minute")
 }
