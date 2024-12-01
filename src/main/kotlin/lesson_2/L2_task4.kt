b3929307363511d4e041405308099ae27b104629
ToothLonely <arsen.shirinov2004@gmail.com>
2024-12-01 18:58:17 +0300

    OnB-2-4 fixed percent

--- src/main/kotlin/lesson_2/L2_task4.kt
+++ src/main/kotlin/lesson_2/L2_task4.kt
@@ -4,15 +4,13 @@ fun main() {
     //Что-то мне подсказывает, что я не правильно понял задание :(
     val crystal: Byte = 7
     val iron: Byte = 11
-    val buff: Float = 0.2f
+    val buff: Byte = 20
     val ironBuff: Byte
-    val crystalBuff: Byte // Как лучше делать? Как делал до этого,
-    //сразу объявлял вычисляемые переменные и инициализировал в нижнем блоке
-    //или объявить их вместе со всеми перменными в первом блоке, а инициализировать в нижнем блоке?
+    val crystalBuff: Byte
 
     //Вычисляю бафф на железо и кристалл
-    ironBuff = (iron * buff).toInt().toByte()
-    crystalBuff = (crystal * buff).toInt().toByte()
+    ironBuff = (iron * buff / 100).toByte()
+    crystalBuff = (crystal * buff / 100).toByte()
 
     println("Кристаллический бафф: $crystalBuff")
     println("Железный бафф: $ironBuff")
