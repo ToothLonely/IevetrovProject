9b95a8884c1a98e7a46dd8612b6427385a869030
ToothLonely <arsen.shirinov2004@gmail.com>
2024-12-02 18:55:13 +0300

    KS-3-5 fix split

--- src/main/kotlin/lesson_3/L3_task5.kt
+++ src/main/kotlin/lesson_3/L3_task5.kt
@@ -2,11 +2,11 @@ package org.example.lesson_3
 
 fun main() {
     val inputMove: String = "D2-D4;0"
-    val stepAndCount = inputMove.split(';')
-    val countMove = stepAndCount[1]
-    val step = stepAndCount[0].split('-')
-    val start = step[0]
-    val finish = step[1]
+    val stepAndCount = inputMove.split(';', '-')
+    val countMove = stepAndCount[2]
+    val start = stepAndCount[0]
+    val finish = stepAndCount[1]
+
     println(start)
     println(finish)
     println(countMove)
