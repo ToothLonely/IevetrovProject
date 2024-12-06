f408f3c8ce793e68b347a6ac55211359230a24e7
ToothLonely <arsen.shirinov2004@gmail.com>
2024-12-06 16:28:41 +0300

    KS-4-2 delete unnecessary comments

--- src/main/kotlin/lesson_4/L4_task2.kt
+++ src/main/kotlin/lesson_4/L4_task2.kt
@@ -1,7 +1,6 @@
 package org.example.lesson_4
 
 fun main() {
-    // Вес 42 кг, объем 120л
     var weight: Byte = 42
     var size: Byte = 120
     println(
@@ -9,7 +8,6 @@ fun main() {
                 "${(weight > 35 && weight <= 100) && (size < 100)}"
     )
 
-    // Вес 20 кг, объем 80 л
     weight = 20
     size = 80
     println(
@@ -17,7 +15,6 @@ fun main() {
                 "${(weight > 35 && weight <= 100) && (size < 100)}"
     )
 
-    //Вес 50 кг, объем 100 л
     weight = 50
     size = 100
     println(
