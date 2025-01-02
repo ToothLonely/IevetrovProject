3b71dbb9e95ded6ea279106d0851fd458764caae
ToothLonely <arsen.shirinov2004@gmail.com>
2025-01-03 01:35:49 +0300

    KS-4-4 removed unnecessary variables

--- src/main/kotlin/lesson_4/L4_task4.kt
+++ src/main/kotlin/lesson_4/L4_task4.kt
@@ -2,13 +2,12 @@ package org.example.lesson_4
 
 fun main() {
     var dayCount: Byte = 5
-    val isArmAndAbdonimalDay: Boolean = dayCount % 2 == 1
     val isBackAndLegDay: Boolean = dayCount % 2 == 0
 
     println(
-        "Упражнения для рук: $isArmAndAbdonimalDay \n" +
+        "Упражнения для рук: ${!isBackAndLegDay} \n" +
         "Упражнения для ног: $isBackAndLegDay \n" +
         "Упражнения для спины: $isBackAndLegDay \n" +
-        "Упражнения для пресса: $isArmAndAbdonimalDay"
+        "Упражнения для пресса: ${!isBackAndLegDay}"
     )
 }
