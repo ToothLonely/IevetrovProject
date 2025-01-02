32634c5cda4f8ea581fb6181355d0df2c7380331
ToothLonely <arsen.shirinov2004@gmail.com>
2025-01-03 01:49:59 +0300

    KS-4-2 removed magic numbers

--- src/main/kotlin/lesson_4/L4_task2.kt
+++ src/main/kotlin/lesson_4/L4_task2.kt
@@ -1,24 +1,28 @@
 package org.example.lesson_4
 
+const val MAXIMUM_WEIGHT = 100
+const val MINIMUM_WEIGHT = 35
+const val MINIMUM_SIZE = 100
+
 fun main() {
     var weight: Byte = 42
     var size: Byte = 120
     println(
         "Груз весом ${weight} кг и объемом ${size} л соотвествует категории 'Average': " +
-                "${(weight > 35 && weight <= 100) && (size < 100)}"
+                "${(weight > MINIMUM_WEIGHT && weight <= MAXIMUM_WEIGHT) && (size < MINIMUM_SIZE)}"
     )
 
     weight = 20
     size = 80
     println(
         "Груз весом ${weight} кг и объемом ${size} л соотвествует категории 'Average': " +
-                "${(weight > 35 && weight <= 100) && (size < 100)}"
+                "${(weight > MINIMUM_WEIGHT && weight <= MAXIMUM_WEIGHT) && (size < MINIMUM_SIZE)}"
     )
 
     weight = 50
     size = 100
     println(
         "Груз весом ${weight} кг и объемом ${size} л соотвествует категории 'Average': " +
-                "${(weight > 35 && weight <= 100) && (size < 100)}"
+                "${(weight > MINIMUM_WEIGHT && weight <= MAXIMUM_WEIGHT) && (size < MINIMUM_SIZE)}"
     )
 }
