22594e1c9225e7491dc505a968650d005349ec2e
ToothLonely <arsen.shirinov2004@gmail.com>
2025-01-03 03:05:26 +0300

    KS-4-5 сhecking the ship's readiness

---
+++ src/main/kotlin/lesson_4/L4_task5.kt
@@ -0,0 +1,23 @@
+package org.example.lesson_4
+
+const val MINIMUM_PASSENGERS: Byte = 55
+const val MAXIMUM_PASSENGERS: Byte = 70
+const val MINIMUM_PROVISION: Byte = 50
+
+fun main() {
+    var isDamage: Boolean = readlnOrNull()!!.toBoolean()
+    var countPassengers: Int = readlnOrNull()!!.toInt()
+    var countProvision: Int = readlnOrNull()!!.toInt()
+    var isGoodWeather: Boolean = readlnOrNull()!!.toBoolean()
+
+    //Проверка базового сценария условий
+    var isBasicFavorableConditions: Boolean =
+        !isDamage && (countPassengers >= MINIMUM_PASSENGERS && countPassengers <= MAXIMUM_PASSENGERS)
+                && (countProvision > MINIMUM_PROVISION) && isGoodWeather
+
+    //Проверка альтернативного сценария условий
+    var isAlternativeFavorableConditions: Boolean =
+        (countPassengers == MAXIMUM_PASSENGERS.toInt()) && (countProvision >= MINIMUM_PROVISION) && isGoodWeather
+
+    println("Можно ли отправляться в путь? ${isAlternativeFavorableConditions || isBasicFavorableConditions}")
+}
