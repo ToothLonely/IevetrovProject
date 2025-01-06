fcd1c4dc2d779cec8d0f93ece6b64d2513c87345
ToothLonely <arsen.shirinov2004@gmail.com>
2025-01-06 23:29:54 +0300

    KS-8-5 create recipe by user input

---
+++ src/main/kotlin/lesson_8/L8_task5.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_8
+
+fun main() {
+    print("Введите кол-во ингредиентов: ")
+    val numberOfIngredients: Int = readln().toInt()
+    val ingredients: Array<String> = Array(numberOfIngredients) { "" }
+
+    //Ввод ингредиентов
+    for (i in 0 until numberOfIngredients) {
+        print("Введите ${i + 1}-й ингредиент: ")
+        ingredients[i] = readln()
+    }
+
+    //Вывод ингредиентов через запятую
+    for (i in 0 until numberOfIngredients) {
+        if (i == ingredients.indexOf(ingredients.last())) {
+            print("${ingredients[i]} ")
+        } else {
+            print("${ingredients[i]}, ")
+        }
+    }
+}
