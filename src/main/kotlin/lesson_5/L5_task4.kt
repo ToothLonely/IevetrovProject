8fb87a3a6c856098993acb8b60e396b957bb1913
ToothLonely <arsen.shirinov2004@gmail.com>
2025-01-23 00:52:20 +0300

    KS-5-4 deleted registration

--- src/main/kotlin/lesson_5/L5_task4.kt
+++ src/main/kotlin/lesson_5/L5_task4.kt
@@ -1,16 +1,11 @@
-package org.example.lesson_5
-
 fun main() {
     val myName = "Zaphod"
     val myPassword = "PanGalactic"
-    val newUserName: String
-    val newUserPassword: String
 
     print("Введите имя пользователя: ")
     val inputName: String = readln()
     val inputPassword: String
 
-    //Аутентификация пользователя
     if (inputName == myName) {
         print("Введите пароль: ")
         inputPassword = readln()
@@ -19,12 +14,7 @@ fun main() {
         } else {
             println("Wrong password!")
         }
-    } else { //Процесс регистрации нового пользователя
-        println("You must sign in...")
-        print("Enter your name: ")
-        newUserName = readln()
-        print("Enter your password: ")
-        newUserPassword = readln()
-        println("registration was successful. Your name: $newUserName, your password: $newUserPassword")
+    } else {
+        println("Wrong name. Please sign in")
     }
 }
