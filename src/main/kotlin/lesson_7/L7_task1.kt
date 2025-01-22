e68c0c18a3fe231e25450cf0b76b6dc30b3fc527
ToothLonely <arsen.shirinov2004@gmail.com>
2025-01-23 01:02:57 +0300

    KS-7-1 put password in val

--- src/main/kotlin/lesson_7/L7_task1.kt
+++ src/main/kotlin/lesson_7/L7_task1.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_7
 
 fun main() {
+    var password = ""
     for (i in 0..5) {
         if (i % 2 == 0) {
-            print((0..9).random())
+            password += (0..9).random()
         } else {
-            print(('a'..'z').random())
+            password += ('a'..'z').random()
         }
     }
+    println(password)
 }
