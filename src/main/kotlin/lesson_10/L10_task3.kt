689e7a46aaf1cf8eebbe0c413acd10a1ce39913f
ToothLonely <arsen.shirinov2004@gmail.com>
2025-01-24 03:24:26 +0300

    KS-10-3 put ranges in variables

--- src/main/kotlin/lesson_10/L10_task3.kt
+++ src/main/kotlin/lesson_10/L10_task3.kt
@@ -3,14 +3,16 @@ package org.example.lesson_10
 import kotlin.random.Random
 
 fun generatePassword(passwordLength: Int): String {
+    val intRange = (0..9)
+    val charRange = (' '..'/')
     var password: String = ""
     var isNumber: Boolean = Random.nextBoolean() //Рандомно выбираю что будет первым, число или спецсимвол
 
     for (i in 0 until passwordLength) {
         if (isNumber) {
-            password += (0..9).random()
+            password += intRange.random()
         } else {
-            password += (' '..'/').random()
+            password += charRange.random()
         }
         isNumber = !isNumber //Меняю значение переменной, чтобы числа и спецсимволы чередовались
     }
