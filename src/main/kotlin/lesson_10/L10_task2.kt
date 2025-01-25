34c1873b72ed8502902d3338eaf78dabd5500c87
ToothLonely <arsen.shirinov2004@gmail.com>
2025-01-26 02:44:17 +0300

    KS-10-2 created simple functions

--- src/main/kotlin/lesson_10/L10_task2.kt
+++ src/main/kotlin/lesson_10/L10_task2.kt
@@ -1,21 +1,19 @@
 package org.example.lesson_10
 
-fun validatePasswordAndLogin(login: String, password: String): Boolean {
-    val passwordLength = 4
-    val loginLength = 4
-    if (login.length < loginLength || password.length < passwordLength) {
-        return false
-    } else {
-        return true
-    }
-}
+fun checkPasswordLength(password: String, minimumPasswordLength: Int) = password.length >= minimumPasswordLength
+
+fun checkLoginLength(login: String, minimumLoginLength: Int) = login.length >= minimumLoginLength
 
 fun main() {
+    val minimumPasswordLength = 4
+    val minimumLoginLength = 4
+
     print("Введите логин (не менее 4 символов): ")
     val login: String = readln()
     print("Введите пароль (не менее 4 символов): ")
     val password: String = readln()
-    if (validatePasswordAndLogin(login, password)) {
+
+    if (checkLoginLength(login, minimumLoginLength) && checkPasswordLength(password, minimumPasswordLength)) {
         println("Добро пожаловать!")
     } else {
         println("Логин или пароль недостаточно длинные")
