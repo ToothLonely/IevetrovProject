43d4ee98069bed72c23fb9f437ca9c77dbf706a3
ToothLonely <arsen.shirinov2004@gmail.com>
2025-01-26 02:58:12 +0300

    KS-9-5 replaced list on set

--- src/main/kotlin/lesson_9/L9_task5.kt
+++ src/main/kotlin/lesson_9/L9_task5.kt
@@ -1,16 +1,14 @@
 package org.example.lesson_9
 
 fun main() {
-    val ingredients: MutableList<String> = mutableListOf()
-    var totalIngredients: Int = 0
+    var totalIngredients = 0
+    val ingredients: MutableSet<String> = mutableSetOf()
 
-    //Заполнение списка с проверкой на уникальность на каждой итерации
     while (totalIngredients < 5) {
         println("Введите ${++totalIngredients}-й ингредиент")
         ingredients.add(readln())
-        if (ingredients.distinct().size != ingredients.size) {
+        if (ingredients.size != totalIngredients) {
             println("Внимание! Каждый ингредиент должен быть уникальным.")
-            ingredients.removeLast()
             totalIngredients--
         }
     }
