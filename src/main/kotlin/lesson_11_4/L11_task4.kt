1c224f67f2dde9ca03fe2ecc8939e3473b81ad8e
ToothLonely <arsen.shirinov2004@gmail.com>
2025-02-13 15:48:06 +0300

    KS-11-4 created entities for recipe app

---
+++ src/main/kotlin/lesson_11_4/L11_task4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_11_4
+
+class RecipeCategory(
+    val listOfRecipes: List<Recipe>,
+    val nameOfCategory: String,
+    val categoryImage: String = "category_image.png",
+) {}
+
+class Recipe(
+    val nameOfRecipe: String,
+    val listOfIngredients: List<Ingredients>,
+    val portions: Int,
+    var isFavorite: Boolean = false,
+    val recipeImage: String = "recipe_image.png",
+) {}
+
+class Ingredients(
+    val nameOfIngredient: String,
+    val amountOfIngredient: Int,
+) {}
