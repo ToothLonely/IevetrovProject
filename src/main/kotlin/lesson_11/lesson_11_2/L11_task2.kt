5dd796b9405e231c451cdc9af8e7f1b626c6c92b
ToothLonely <arsen.shirinov2004@gmail.com>
2025-02-13 13:14:07 +0300

    KS-11-2 created class User2 with password and bio

---
+++ src/main/kotlin/lesson_11/lesson_11_2/L11_task2.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_11.lesson_11_2
+
+fun main() {
+    val Ivan = User2(3, "Ivan", "qwerty", "Ivan@mail.ru")
+
+    Ivan.setBio()
+    Ivan.setPassword()
+    Ivan.printProperties()
+}
