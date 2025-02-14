626d22fab650b5d9aa4ad6ad5a5e8d0bd60fb6e4
ToothLonely <arsen.shirinov2004@gmail.com>
2025-02-14 13:33:35 +0300

    KS-11-1 deleted unnecessary file

--- src/main/kotlin/lesson_11/lesson_11_1/L11_task1.kt
+++ src/main/kotlin/lesson_11/lesson_11_1/L11_task1.kt
@@ -1,5 +1,12 @@
 package org.example.lesson_11.lesson_11_1
 
+class User(
+    val id: Int,
+    val login: String,
+    val password: Int,
+    val mail: String,
+)
+
 fun main() {
     val Tarakan = User(1, "Tarakan", 12345, "tarakan@mail.ru")
     val Klop = User(2, "Klop", 67890, "Klop@mail.ru")
--- src/main/kotlin/lesson_11/lesson_11_1/User.kt
+++
@@ -1,8 +0,0 @@
-package org.example.lesson_11.lesson_11_1
-
-class User(
-    val id: Int,
-    val login: String,
-    val password: Int,
-    val mail: String,
-)
