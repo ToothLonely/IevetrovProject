dfcfa237a6bb6c00cb70db02964a6e96964d2b76
ToothLonely <arsen.shirinov2004@gmail.com>
2025-03-12 22:54:31 +0300

    KS-17-2 created object using constructor

--- src/main/kotlin/lesson_17/L17_task2.kt
+++ src/main/kotlin/lesson_17/L17_task2.kt
@@ -1,23 +1,29 @@
 package org.example.lesson_17
 
-class Ship {
-    var name: String = "Победа"
+class Ship(
+    _name: String,
+    _port: String,
+    _avgSpeed: Double
+) {
+    var name: String = _name
         set(value) {
             println("Имя нельзя поменять!")
         }
-    var port: String = "Новороссийск"
-    var avgSpeed: Double = 99.9
+    var port: String = _port
+    var avgSpeed: Double = _avgSpeed
+
+    fun printInfo() {
+        println("Название корабля: $name\nПорт приписки: $port\nСредняя скорость: $avgSpeed")
+    }
 
 }
 
 fun main() {
-    val ship = Ship().apply {
-        name = "Беда"
-        port = "Архангельск"
-        avgSpeed = 199.9
-    }.run {
-        println("Название корабля: $name")
-        println("Порт приписки: $port")
-        println("Средняя скорость: $avgSpeed")
-    }
+    val ship = Ship("Победа", "Новороссийск", 99.9)
+
+    ship.name = "Беда"
+    ship.port = "Архангельск"
+    ship.avgSpeed = 199.9
+
+    ship.printInfo()
 }
